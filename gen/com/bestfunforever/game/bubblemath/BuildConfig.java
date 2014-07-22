1c633a778afebe89482b9462b227bdcec67599a3
tgioihan <tgioihan@gmail.com>
2014-07-22 10:46:26 +0700

    initial

---
+++ gen/com/bestfunforever/game/bubblemath/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.bestfunforever.game.bubblemath;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/bestfunforever/game/bubblemath/BubbleGameActivity.java
@@ -0,0 +1,5 @@
+package com.bestfunforever.game.bubblemath;
+
+public class BubbleGameActivity extends{
+
+}
