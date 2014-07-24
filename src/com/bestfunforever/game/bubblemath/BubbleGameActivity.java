7954e4747a410eda1f10e82ae526b7a2c122a934
tgioihan <tgioihan@gmail.com>
2014-07-24 09:51:47 +0700

    cm

--- src/com/bestfunforever/game/bubblemath/BubbleGameActivity.java
+++ src/com/bestfunforever/game/bubblemath/BubbleGameActivity.java
@@ -4,16 +4,26 @@ import org.andengine.engine.camera.hud.HUD;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.SpriteBackground;
 import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.text.AutoWrap;
+import org.andengine.entity.text.TickerText.TickerTextOptions;
 import org.andengine.entity.util.FPSLogger;
+import org.andengine.opengl.font.Font;
+import org.andengine.opengl.font.FontFactory;
+import org.andengine.opengl.font.StrokeFont;
 import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.region.TextureRegion;
 import org.andengine.opengl.texture.region.TiledTextureRegion;
+import org.andengine.util.HorizontalAlign;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.widget.Toast;
 
 import com.bestfunforever.andengine.uikit.activity.PortraitAdmobGameActivity;
+import com.bestfunforever.andengine.uikit.entity.TickerTextManagable;
+import com.bestfunforever.andengine.uikit.entity.TickerTextManagable.ITickerTextListenner;
 import com.bestfunforever.andengine.uikit.menu.BaseMenu.IOnMenuItemClickListener;
 import com.bestfunforever.andengine.uikit.menu.IMenuItem;
 
@@ -24,6 +34,9 @@ public class BubbleGameActivity extends PortraitAdmobGameActivity implements IOn
 	private TiledTextureRegion childFaceRegion;
 	private TextureRegion mBgTextureRegion;
 	private TiledTextureRegion messageRegion;
+	private Font mFont;
+	private BitmapTextureAtlas customFontTexture;
+	private StrokeFont customFont;
 
 	@Override
 	protected void onCreateResources() {
@@ -51,6 +64,15 @@ public class BubbleGameActivity extends PortraitAdmobGameActivity implements IOn
 		this.mBgTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(mBgBitmapTextureAtlas, this,
 				"bg5.jpg", 0, 0); // 64x32
 		mBgBitmapTextureAtlas.load();
+		
+		customFontTexture = new BitmapTextureAtlas(this.getTextureManager(),512, 512, TextureOptions.BILINEAR);
+		FontFactory.setAssetBasePath("font/");
+		customFont = FontFactory.createStrokeFromAsset(getFontManager(),customFontTexture , getAssets(), "UVNBanhMi.TTF", (float)50*ratio, true, Color.WHITE, 2, Color.RED);
+		customFont.load();
+		
+		this.mFont = FontFactory.create(this.getFontManager(), this.getTextureManager(), (int) (256 * ratio),
+				(int) (256 * ratio), Typeface.create(Typeface.DEFAULT, Typeface.BOLD), (int) (32 * ratio));
+		this.mFont.load();
 	}
 
 	@Override
@@ -60,7 +82,7 @@ public class BubbleGameActivity extends PortraitAdmobGameActivity implements IOn
 				getVertexBufferObjectManager())));
 		this.mEngine.registerUpdateHandler(new FPSLogger());
 
-		mListView = new AutoScrollHorizontalList(this, 10, 200, CAMERA_WIDTH - 20, iconHighScoreMenuRegion.getHeight()
+		mListView = new AutoScrollHorizontalList(this, 10, 50, CAMERA_WIDTH - 20, iconHighScoreMenuRegion.getHeight()
 				* ratio, getVertexBufferObjectManager());
 		MathAdapter adapter = new MathAdapter(iconHighScoreMenuRegion, ratio, getVertexBufferObjectManager());
 		scene.attachChild(mListView);
@@ -81,7 +103,16 @@ public class BubbleGameActivity extends PortraitAdmobGameActivity implements IOn
 				- messageRegion.getHeight() * ratio, messageRegion.getWidth() * ratio, messageRegion.getHeight()
 				* ratio, messageRegion, getVertexBufferObjectManager());
 		scene.attachChild(messageFrame);
-
+		
+		TickerTextManagable tickTextManagable = new TickerTextManagable(10*ratio, 10*ratio, mFont, "Giup minh giai bai toan nay voi , kho qua , hu hu", new TickerTextOptions(AutoWrap.WORDS, messageFrame.getWidth()-2*10*ratio, HorizontalAlign.CENTER, 15), getVertexBufferObjectManager());
+		tickTextManagable.setTickerTextListenner(new ITickerTextListenner() {
+			
+			@Override
+			public void onTickerTextComplete() {
+				mListView.setRun(true);
+			}
+		});
+		messageFrame.attachChild(tickTextManagable);
 		return scene;
 	}
 
